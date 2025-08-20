7d9d804c757f02155750d6821ad9ac070f837971
amankumar0806 <105356424+amankumar0806@users.noreply.github.com>
2025-08-20 20:46:05 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-Docker pull awsdevops0806/simple-python-flask-app
+docker pull awsdevops0806/simple-python-flask-app
 
 # Run the Docker image as a container
-Docker run -d -p 5000:5000 awsdevops0806/simple-python-flask-app
+docker run -d -p 5000:5000 awsdevops0806/simple-python-flask-app
